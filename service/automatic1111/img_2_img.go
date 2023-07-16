60809f18722a40391b12c748fb49a16b02bc61a0
sebas <sebas.arriola@ibexmercado.com>
2023-07-16 10:18:05 -0600

    use env

--- service/automatic1111/img_2_img.go
+++ service/automatic1111/img_2_img.go
@@ -1,15 +1,16 @@
 package automatic1111
 
 import (
+	"fmt"
 	"github.com/lnconsole/photobolt/http"
 )
 
-func Img2Img(input *Img2ImgInput) (*ImgOutput, error) {
+func Img2Img(automatic1111Url string, input *Img2ImgInput) (*ImgOutput, error) {
 	var (
 		output = &ImgOutput{}
 	)
 
-	if err := http.Post("http://127.0.0.1:7860/sdapi/v1/img2img", input, output); err != nil {
+	if err := http.Post(fmt.Sprintf("%s/sdapi/v1/img2img", automatic1111Url), input, output); err != nil {
 		return nil, err
 	}
 
--- service/automatic1111/text_2_img.go
+++ service/automatic1111/text_2_img.go
@@ -1,15 +1,16 @@
 package automatic1111
 
 import (
+	"fmt"
 	"github.com/lnconsole/photobolt/http"
 )
 
-func Text2Img(input *Text2ImgInput) (*ImgOutput, error) {
+func Text2Img(automatic1111Url string, input *Text2ImgInput) (*ImgOutput, error) {
 	var (
 		output = &ImgOutput{}
 	)
 
-	if err := http.Post("http://127.0.0.1:7860/sdapi/v1/txt2img", input, output); err != nil {
+	if err := http.Post(fmt.Sprintf("%s/sdapi/v1/txt2img", automatic1111Url), input, output); err != nil {
 		return nil, err
 	}
 
